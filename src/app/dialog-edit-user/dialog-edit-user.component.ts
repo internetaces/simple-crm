80bc3d6a18a7bbe215c4381487f10e2390be81da
Marcel Zilkowski <info@internetaces.com>
2023-12-17 10:22:01 +0100

    User edit function for current ID

--- src/app/dialog-edit-user/dialog-edit-user.component.ts
+++ src/app/dialog-edit-user/dialog-edit-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { User } from 'src/models/user.class';
+import { Firestore, addDoc, collection, collectionData, doc, updateDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -8,6 +9,8 @@ import { Observable } from 'rxjs';
   styleUrls: ['./dialog-edit-user.component.scss']
 })
 export class DialogEditUserComponent implements OnInit {
+  firestore: Firestore = inject(Firestore)	
+  items$: Observable<any[]>;	
   user: User;
   userId:string;
   birthDate: Date;
@@ -22,8 +25,14 @@ export class DialogEditUserComponent implements OnInit {
     }
   
   
-    saveUser(){
-      
-    }
+ //User updaten
+ saveUser(){
+  console.log(this.user);
+this.loading = true;
+
+const coll = doc(this.firestore, 'users', this.userId);
+updateDoc(coll, this.user.toJason());
+   this.loading = false;
+ }
 
 }
